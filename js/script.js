e952a44682c77d2d284000dfeb59dc9bdb39417e
Andrea <andreagutierrezromero98@gmail.com>
2024-11-24 12:15:37 +0100

    botones eliminar ok

--- js/script.js
+++ js/script.js
@@ -24,90 +24,71 @@ botonIdChiste.addEventListener("click", function() {
 
 //HACEMOS LA FUNCION DE RENDERIZAR EL CHISTE Y EL DESTRUCTURING
 function renderizarChiste(chiste) {
-    const {id, value, icon_url} = chiste;
+    const {id, value,} = chiste;
     const copiaChiste = {...chiste};
 
 //CREAMOS LOS ELEMENTOS NECESARIOS LI, DIV, P,  EL BOTON DE ELIMINAR
 // Y HACEMOS OTRA FUNCION CON EL ADDEVENTLISTENER
 const liElement = document.createElement("li");
-liElement.id = id;
+  liElement.id = id;
 
-const divContainer = document.createElement("div");
-divContainer.classList.add("divContainer");
+  const divContainer = document.createElement("div");
+  divContainer.classList.add("divContainer");
 
-const parrafo = document.createElement("p");
-parrafo.textContent = value;
+  const parrafo = document.createElement("p");
+  parrafo.textContent = value;
 
-const botonEliminar = document.createElement("button");
-botonEliminar.id = id;
-botonEliminar.textContent = "Eliminar";
-botonEliminar.addEventListener("click", function() {
-//LUEGO EN EL LOCALSTORAGE PARA REMOVER ESE BOTONELIMINAR
-localStorage.removeItem(botonEliminar.id);
-//ESTO ELIMINA EL ITEM DESDE EL LOCALSTORAGE
-const liRemove = document.getElementById("botonEliminar.id");
-if (liRemove) {
-    liRemove.remove(); //ELIMINA EL LI ELEMENTO DESDE EL DOM
+  const botonEliminar = document.createElement("button");
+  botonEliminar.id = id;
+  botonEliminar.textContent = "Eliminar";
+  botonEliminar.addEventListener("click", function () {
+    localStorage.removeItem(id); 
+    liElement.remove(); 
+  });
+
+  divContainer.appendChild(parrafo);
+  divContainer.appendChild(botonEliminar);
+  liElement.appendChild(divContainer);
+  ulId.appendChild(liElement);
+
+  guardarLocalStore(id, copiaChiste);
 }
-});
 
-divContainer.appendChild(parrafo);
-divContainer.appendChild(botonEliminar);
-liElement.appendChild(divContainer);
-ulId.appendChild(liElement);
-guardarLocalStore(id, copiaChiste);
-
-}
-//LUEGO HACEMOS OTRA FUNCION PARA GUARDAR EL CHISTE (ID Y CHISTE) EN EL LOCALSTORAGE
+// GUARDAR CHISTE LOCALSTORAGE
 function guardarLocalStore(id, chiste) {
-    localStorage.setItem(id, JSON.stringify(chiste)); //GUARDA LA BROMA EN EL LOCALSTORAGE
-
+  localStorage.setItem(id, JSON.stringify(chiste)); 
 }
 
-//LUEGO HACEMOS OTRA FUNCION PARA CARGAR LA LISTA EN EL LOCALSTORE
+
+/// Función para cargar la lista desde el localStorage
 function cargarListaLocalStore() {
-    const keys = Object.entries(localStorage);
-    //HACEMOS EL METODO OBJETO DEL ENTRIES PARA DEVOLVER
-    //EL ARRAY DE LOS ARRAYS
-    for (let key of keys) {
-        const clave = key[0];
-        const valor = JSON.parse(key[1]);
+    const keys = Object.keys(localStorage); 
+    keys.forEach((key) => {
+      const valor = JSON.parse(localStorage.getItem(key));
+      renderizarChiste(valor); 
+    });
+  }
+  
+  // ELIMINAMOS CHISTES GUARDADOS - DOM
+  function BotonlimpiarlocalStore() {
     
-    if (valor.icon_url === 
-        'https://api.chucknorris.io/img/avatar/chuck-norris.png') {
-            renderizarChiste(valor);
-        }    
-    }
-
-}
-//CREAMOS UN BOTON PARA ELIMINAR TODOS LOS ELEMENTOS A LA VEZ
-function BotonlimpiarlocalStore() {
-    //creamos en el DOM un boton para eliminar todo el Localstorage
     const botonEliminarTodo = document.createElement("button");
     botonEliminarTodo.classList.add("EliminarTodo");
     botonEliminarTodo.textContent = "Eliminar todo";
-
-//Agregamos el manejador al boton
-botonEliminarTodo.addEventListener("click", function() {
-    for (let i = localStorage.length -1; i >= 0; i--) {
-        const key = localStorage.key(i);
-        const value =
-    JSON.parse(localStorage.getItem(key));
-    if (value && value.icon_url) {
-        localStorage.removeItem(key);
-        const liRemove =
-    document.getElementById(key);
-    if (liRemove) {
-        liRemove.remove();
-
-     }    
-
-   } 
- }
-});
-
-//AÑADIMOS el botonEliminartodo en el body 
-elementBody.appendChild(botonEliminarTodo)
-}
-cargarListaLocalStore();
-BotonlimpiarlocalStore();
+  
+  
+    botonEliminarTodo.addEventListener("click", function () {
+      
+      localStorage.clear(); // Limpiamos localstorage
+      ulId.innerHTML = ''; // Limpiamos DOM
+    });
+  
+    // Boton al body
+    elementBody.appendChild(botonEliminarTodo);
+  }
+  
+  //  cargamos la lista de chistes al inicio
+  cargarListaLocalStore();
+  
+  // Creamos botón de "Eliminar todo"
+  BotonlimpiarlocalStore();
